77416356bb81e4c942efb579ac074e27a066b82f
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 10:24:13 -0300

    feat: ensure Dexterity quality degrades twice when sellIn date expires

--- src/items/dexterity.ts
+++ src/items/dexterity.ts
@@ -6,9 +6,11 @@ export class Dexterity extends Item implements IUpdateQuality {
         super('+5 Dexterity Vest', sellIn, quality)
     }
     updateQuality(): void {
-        if(this.quality > 0){
+        if(this.quality > 0 && this.sellIn >= 0){
             this.quality -=1
         }
+        else if(this.quality > 0 && this.sellIn < 0 )
+            this.quality -= 2
         this.sellIn -=1
     }
 }
