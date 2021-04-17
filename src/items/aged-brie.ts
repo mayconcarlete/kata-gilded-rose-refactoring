1e8f68ab40b12720d60282a5f4dba5c28b0590ea
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 16:52:14 -0300

    feat: ensure Aged Brie increase quality the older it gets

---
+++ src/items/aged-brie.ts
@@ -0,0 +1,12 @@
+import { IUpdateQuality } from "../interfaces/update-quality";
+import {Item} from '../model/item'
+
+export class AgedBrie extends Item implements IUpdateQuality{
+    constructor(sellIn:number, quality: number){
+        super('Aged Brie', sellIn, quality)
+    }
+    updateQuality(): void {
+        this.quality < 50 ? this.quality +=1 : this.quality
+        this.sellIn -= 1
+    }
+}
