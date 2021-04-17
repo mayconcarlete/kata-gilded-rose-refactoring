db67a5f9cc90b685b4934448e4917e87c25c4ce6
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 16:01:07 -0300

    feat: ensure Elixir return 0 when quality should be negative

--- src/items/elixir.ts
+++ src/items/elixir.ts
@@ -6,9 +6,8 @@ export class Elixir extends Item implements IUpdateQuality{
         super('Elixir of mongoose', sellIn, quality)
     }
     updateQuality(): void {
-        if(this.quality > 0 && this.sellIn >=0){
-            this.quality -= 1
-        }
+        this.sellIn >= 0 ? this.quality -= 1 : this.quality -= 2
+        this.quality < 0 ? this.quality = 0 : this.quality
         this.sellIn -= 1
     }
 }
