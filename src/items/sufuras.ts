19db7faba4f41dfa9b5994406ed11c957fad8a58
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 16:33:25 -0300

    feat: ensure Sufuras remains with the same quality when sellIn pass

---
+++ src/items/sufuras.ts
@@ -0,0 +1,11 @@
+import { IUpdateQuality } from "../interfaces/update-quality";
+import {Item} from '../model/item'
+
+export class Sufuras extends Item implements IUpdateQuality{
+    constructor(sellIn:number, quality: number){
+        super('Sulfuras, Hand of Ragnaros', sellIn, quality)
+    }
+    updateQuality(): void {
+        this.sellIn -= 1
+    }
+}
