017ffd1a77bff603d585ca6793fd368262ed61e4
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 10:47:16 -0300

    test: ensure Dexterity quality returns 0 when quality should be negative

--- tests/dexterity.spec.ts
+++ tests/dexterity.spec.ts
@@ -8,7 +8,7 @@ describe('+5 Dexterity Item', () => {
       expect(dexterityItem.sellIn).toBe(0)        
     })
     test('Should degrades  +5 Dexiterity Vest quality twice when sellIn is negative', () => {
-       const dexterityItem:Dexterity = new Dexterity(-1, 2)
+       const dexterityItem:Dexterity = new Dexterity(-1, 1)
        dexterityItem.updateQuality()
        expect(dexterityItem.quality).toBe(0)
        expect(dexterityItem.sellIn).toBe(-2)
