a9360398b4d965a3459849285f3e27a20332e049
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 17:17:37 -0300

    feat: ensure BackstaegPasses not allow quality over than 50 when sellIn decrease

--- src/items/backstaged-passes.ts
+++ src/items/backstaged-passes.ts
@@ -7,6 +7,7 @@ export class BackstagedPasses extends Item implements IUpdateQuality{
     updateQuality(): void {
         this.sellIn > 5 && this.sellIn <= 10 ? this.quality += 2 : this.quality
         this.sellIn <= 5 && this.sellIn >= 0 ? this.quality +=3 : this.quality
+        this.quality > 50 ? this.quality = 50 : this.quality
         this.sellIn -= 1
     }
 }
