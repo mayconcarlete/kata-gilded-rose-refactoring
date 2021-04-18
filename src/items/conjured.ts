f1e1404caa7deae8cbc4feb63af20bff3a498914
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 21:40:23 -0300

    feat: ensure Conjured decrease by 1 when sellIn is greater than 0

---
+++ src/items/conjured.ts
@@ -0,0 +1,12 @@
+import { IUpdateQuality } from "../interfaces/update-quality";
+import {Item} from '../model/item'
+
+export class Conjured extends Item implements IUpdateQuality{
+    constructor(sellIn: number, quality: number){
+        super('Conjured', sellIn, quality)
+    }
+    updateQuality(): void {
+        this.sellIn >= 0 ? this.quality -= 1: this.quality
+        this.sellIn -= 1
+    }
+}
