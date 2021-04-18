18bd96145b8fd7a5a4a095a91eab8b070f98cf76
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 22:01:39 -0300

    test: ensure Elixir not return negative quality

--- tests/elixir.spec.ts
+++ tests/elixir.spec.ts
@@ -1,16 +1,22 @@
 import { Elixir } from '../src/items/elixir'
 
 describe('Elixir Item', () => {
-  test('Should not allow negative quality in item', () => {
-    const elixirItem: Elixir = new Elixir(0, 0)
+  test('Should decrease Dexterity quality by one when sellIn updates', () => {
+    const elixirItem: Elixir = new Elixir(1, 1)
     elixirItem.updateQuality()
+    expect(elixirItem.sellIn).toBe(0)
     expect(elixirItem.quality).toBe(0)
-    expect(elixirItem.sellIn).toBe(-1)
   })
   test('Should decrease Dexterity quality by two when sellIn expires', () => {
     const elixirItem: Elixir = new Elixir(-1, 1)
     elixirItem.updateQuality()
     expect(elixirItem.quality).toBe(0)
     expect(elixirItem.sellIn).toBe(-2)
+  })
+  test('Should not allow negative quality in item', () => {
+    const elixirItem: Elixir = new Elixir(0, 0)
+    elixirItem.updateQuality()
+    expect(elixirItem.quality).toBe(0)
+    expect(elixirItem.sellIn).toBe(-1)
   })
 })
