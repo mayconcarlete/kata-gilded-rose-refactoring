3871b79e2aca705879665ba41a593d04377c910c
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 22:01:13 -0300

    test: ensure Dexterity not return negative quality

--- tests/dexterity.spec.ts
+++ tests/dexterity.spec.ts
@@ -1,16 +1,22 @@
 import { Dexterity } from '../src/items/dexterity'
 
 describe('+5 Dexterity Item', () => {
+  test('Should decrease +5 Dexiterity by one when sellIn updates', () => {
+    const dexterityItem: Dexterity = new Dexterity(1,1)
+    dexterityItem.updateQuality()
+    expect(dexterityItem.quality).toBe(0)
+    expect(dexterityItem.sellIn).toBe(0)
+  })
+  test('Should decrease +5 Dexiterity Vest quality twice when sellIn expires', () => {
+    const dexterityItem: Dexterity = new Dexterity(-1, 1)
+    dexterityItem.updateQuality()
+    expect(dexterityItem.quality).toBe(0)
+    expect(dexterityItem.sellIn).toBe(-2)
+  })
   test('Should not return negative quality for +5 Dexterity Vest', () => {
     const dexterityItem: Dexterity = new Dexterity(1, 0)
     dexterityItem.updateQuality()
     expect(dexterityItem.quality).toBe(0)
     expect(dexterityItem.sellIn).toBe(0)
-  })
-  test('Should degrades  +5 Dexiterity Vest quality twice when sellIn expires', () => {
-    const dexterityItem: Dexterity = new Dexterity(-1, 1)
-    dexterityItem.updateQuality()
-    expect(dexterityItem.quality).toBe(0)
-    expect(dexterityItem.sellIn).toBe(-2)
   })
 })
