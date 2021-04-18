77ce42d2882d753ce83677fac1c4b860580181ca
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 23:32:57 -0300

    feat: ensure BackstagedPasses increase by 1 if sellIn is greater than 10

--- src/items/backstaged-passes.ts
+++ src/items/backstaged-passes.ts
@@ -6,6 +6,7 @@ export class BackstagedPasses extends Item implements IUpdateQuality {
   }
 
   updateQuality (): void {
+    this.sellIn > 10 ? this.quality += 1 : this.quality
     this.sellIn > 5 && this.sellIn <= 10 ? this.quality += 2 : this.quality
     this.sellIn <= 5 && this.sellIn >= 0 ? this.quality += 3 : this.quality
     this.quality > 50 ? this.quality = 50 : this.quality
