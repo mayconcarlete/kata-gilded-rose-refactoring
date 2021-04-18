7d1223b0913b670b42d24547719343152cc76b4a
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-17 23:33:02 -0300

    test: ensure BackstagedPasses increase by 1 if sellIn is greater than 10

--- tests/backstage-passes.test.ts
+++ tests/backstage-passes.test.ts
@@ -1,6 +1,12 @@
 import { BackstagedPasses } from '../src/items/backstaged-passes'
 
 describe('Backstage Passes Item', () => {
+  test('Should Backstage Passes increase quality by 1 when sellIn is greater than 10', () => {
+    const backstagedPasses:BackstagedPasses = new BackstagedPasses(13, 10)
+    backstagedPasses.updateQuality()
+    expect(backstagedPasses.sellIn).toBe(12)
+    expect(backstagedPasses.quality).toBe(11)
+  })
   test('should Backstage Passes increase quality by 2 when sellIn is less than 10 and more than 5', () => {
     const backstagedPasses: BackstagedPasses = new BackstagedPasses(9, 10)
     backstagedPasses.updateQuality()
